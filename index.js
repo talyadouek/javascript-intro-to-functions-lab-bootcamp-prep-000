b8bd3e62706700fa0909f634d3f42f43f1ff76ea
Talya D <talyadouek@gmail.com>
2018-04-10 23:14:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function shout(string) {
+<<<<<<< HEAD
   return string.toUpperCase() ;
 }
 function whisper(string) {
@@ -19,3 +20,24 @@ function sayHiToGrandma(string) {
        return "I love you, too." ; 
      }
 }
+=======
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) { 
+    if (lowercase.toLowerCase() === lowercase) {
+    return "I can\t hear you!"}
+      }
+      else if (uppercase.toUpperCase()===uppercase) {
+        return "YES INDEED!" } 
+        else "I love you too" ; {
+       }   
+>>>>>>> 02b1fae597f697d1f75d553c5ce6e1ff0a2cca8b
